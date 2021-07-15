c8c2183fd6069cc57e468e76c7c7b3a9ceb23c82
Hui Xue <hui.xue@nih.gov>
2021-07-15 18:09:48 -0400

    update src/a2_pytorch_mlp.py

--- assignments/assignment2/src/a2_pytorch_mlp.py
+++ assignments/assignment2/src/a2_pytorch_mlp.py
@@ -31,29 +31,7 @@ from torch.utils.data import Dataset, DataLoader, sampler
 import torchvision.transforms as transforms
 
 class PytorchMLP(nn.Module):
-    # def __init__(self, num_hidden_layers=[300, 200, 100]):
-    #     super().__init__()
-        
-    #     # *** START CODE HERE ***
-    #     self.conv1 = nn.Conv2d(3, 6, 5)
-    #     self.pool = nn.MaxPool2d(2, 2)
-    #     self.conv2 = nn.Conv2d(6, 16, 5)
-    #     self.fc1 = nn.Linear(16 * 5 * 5, 120)
-    #     self.fc2 = nn.Linear(120, 84)
-    #     self.fc3 = nn.Linear(84, 10)
-    #     # *** END CODE HERE ***
 
-    # def forward(self, x):
-    #     # *** START CODE HERE ***
-    #     x = self.pool(F.relu(self.conv1(x)))
-    #     x = self.pool(F.relu(self.conv2(x)))
-    #     x = torch.flatten(x, 1) # flatten all dimensions except batch
-    #     x = F.relu(self.fc1(x))
-    #     x = F.relu(self.fc2(x))
-    #     x = self.fc3(x)
-    #     # *** END CODE HERE ***
-    #     return x
-    
     def __init__(self, H, W, C, num_hidden_layers):
         """Initial the model
 
